2d98277fed1e0ed8416b529aeaca0a9bc157aa05
Ayoub Taouam <ayoubtaouam@gmail.com>
2025-05-01 15:24:43 +0100

    Add download file method.

--- src/main/java/com/titlichargi/downloader/controller/DownloadController.java
+++ src/main/java/com/titlichargi/downloader/controller/DownloadController.java
@@ -1,9 +1,17 @@
 package com.titlichargi.downloader.controller;
 
 import com.titlichargi.downloader.service.DownloadService;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 @RestController
@@ -20,5 +28,21 @@ public class DownloadController {
     public ResponseEntity<String> download(@RequestBody Map<String, String> body) {
         String url = body.get("url");
         return downloadService.downloadVideo(url);
+    }
+
+    @GetMapping("file/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        try {
+            Path path = Paths.get("download").resolve(filename).normalize();
+            InputStream is = Files.newInputStream(path);
+            InputStreamResource resource = new InputStreamResource(is);
+            return ResponseEntity.ok().
+                    contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                    .body(resource);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
