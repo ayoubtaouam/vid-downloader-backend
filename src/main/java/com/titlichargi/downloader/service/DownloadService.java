fd5c7ef88103cc6466b06808e36a7721b6c0be05
Ayoub Taouam <ayoubtaouam@gmail.com>
2025-05-30 16:12:08 +0100

    Send the file name to the client in a json response.

--- src/main/java/com/titlichargi/downloader/service/DownloadService.java
+++ src/main/java/com/titlichargi/downloader/service/DownloadService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -14,14 +15,15 @@ import java.util.Map;
 public class DownloadService {
     public ResponseEntity<Map<String, String>> downloadVideo(String url) throws IOException, InterruptedException {
         Path downloadPath = Paths.get("download");
-        Process process = new ProcessBuilder("yt-dlp", "--print", "filename", "-P", "download", url).start();
+        Process process = new ProcessBuilder("yt-dlp", "-P", downloadPath.toString(), "--no-simulate", "--print", "filename", url).start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String fullPath = reader.readLine();
         Path path = Paths.get(fullPath);
         String filename = path.getFileName().toString();
         Map<String, String> response = Map.of(
                 "message", "Download successful",
-                "filename", filename
+                "filename", filename,
+                "path", fullPath
         );
         int exitCode = process.waitFor();
         if (exitCode == 0) {
