e996dc8cbad3a1077154bce0c611919685472271
Ayoub Taouam <ayoubtaouam@gmail.com>
2025-06-04 16:34:37 +0100

    Refactoring the service so know it only gets the filename, then using this file name in the response headers in order to get the filename while downloading it, and clean the old endpoints.

--- src/main/java/com/titlichargi/downloader/controller/DownloadController.java
+++ src/main/java/com/titlichargi/downloader/controller/DownloadController.java
@@ -2,25 +2,15 @@ package com.titlichargi.downloader.controller;
 
 import com.titlichargi.downloader.service.DownloadService;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", exposedHeaders = "Content-Disposition")
 public class DownloadController {
     private final DownloadService downloadService;
 
@@ -28,19 +18,13 @@ public class DownloadController {
         this.downloadService = downloadService;
     }
 
-//    @PostMapping("download")
-//    public ResponseEntity<Map<String, String>> download(@RequestBody Map<String, String> body) throws IOException, InterruptedException {
-//        String url = body.get("url");
-//        return downloadService.downloadVideo(url);
-//    }
-
     @PostMapping("download")
     public void download(@RequestBody Map<String, String> body, HttpServletResponse response) throws Exception {
-        Path path = Paths.get("download");
         String url = body.get("url");
-        Process process = new ProcessBuilder("yt-dlp", "-P", path.toString(), "-o", "-", url).start();
+        String filename = downloadService.getFilename(url);
+        Process process = new ProcessBuilder("yt-dlp", "-o", "-", url).start();
         response.setContentType("video/mp4");
-        response.setHeader("content-disposition", "attachment; filename=\"video.mp4\"");
+        response.setHeader("Content-Disposition", "attachment; filename=" + filename);
 
         try(
                 InputStream is = process.getInputStream();
@@ -57,18 +41,5 @@ public class DownloadController {
                 throw new Exception();
             }
         }
-    }
-
-    @GetMapping("file")
-    public ResponseEntity<Resource> getFile(@RequestParam String filename) throws IOException {
-
-        Path path = Paths.get("download").resolve(filename).normalize();
-        System.out.println(path);
-        InputStream is = Files.newInputStream(path);
-        InputStreamResource resource = new InputStreamResource(is);
-        return ResponseEntity.ok().
-                contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
-                .body(resource);
     }
 }
--- src/main/java/com/titlichargi/downloader/service/DownloadService.java
+++ src/main/java/com/titlichargi/downloader/service/DownloadService.java
@@ -13,26 +13,17 @@ import java.util.Map;
 
 @Service
 public class DownloadService {
-    public ResponseEntity<Map<String, String>> downloadVideo(String url) throws IOException, InterruptedException {
-        Path downloadPath = Paths.get("download");
-        Process process = new ProcessBuilder("yt-dlp", "-P", downloadPath.toString(), "--no-simulate", "--print", "filename", url).start();
+    public String getFilename(String url) throws Exception {
+        Process process = new ProcessBuilder("yt-dlp", "--print", "filename", url).start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String fullPath = reader.readLine();
         Path path = Paths.get(fullPath);
         String filename = path.getFileName().toString();
-        Map<String, String> response = Map.of(
-                "message", "Download successful",
-                "filename", filename,
-                "path", fullPath
-        );
         int exitCode = process.waitFor();
         if (exitCode == 0) {
-            return ResponseEntity.ok(response);
+            return filename;
         } else {
-            Map<String, String> error = Map.of(
-                    "error", "Download failed with exit code: " + exitCode
-            );
-            return ResponseEntity.status(500).body(error);
+            throw new Exception("can't get filename");
         }
     }
 
